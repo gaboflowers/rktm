995117d371468fd9fb5d76060e285b265489b6f7
gaboflowers <gabriel.flores.m@ug.uchile.cl>
2020-07-29 05:18:58 -0400

    Check for Racket

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@ RKTMDIR="$DESTDIR/lib/rktm"
 TARGETS="lexer.rkt parser.rkt transpiler.rkt template.c"
 BINDIR="$DESTDIR/bin"
 
+if ! command -v racket &> /dev/null; then
+    echo "Install racket first."
+    exit 1
+fi
+
 if [ -d "$RKTMDIR" ]; then
     echo "$RKTMDIR already exists. Overwriting contents."
 else
