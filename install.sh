a8e3570da284d4a29189867905f22c5314f5b18b
gaboflowers <gabriel.flores.m@ug.uchile.cl>
2020-07-29 03:35:43 -0400

    Installation additional message

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 DESTDIR=/usr
 RKTMDIR="$DESTDIR/lib/rktm"
 TARGETS="lexer.rkt parser.rkt transpiler.rkt template.c"
-RKTMBIN="$DESTDIR/bin/"
+BINDIR="$DESTDIR/bin"
 
 if [ -d "$RKTMDIR" ]; then
     echo "$RKTMDIR already exists. Overwriting contents."
@@ -10,5 +10,18 @@ else
     mkdir -m 755 -p "$RKTMDIR" || exit 1
 fi
 
+if [ ! -d "$BINDIR" ]; then
+    mkdir -m 755 -p "$BINDIR" || exit 1
+fi
+
 cp $(echo "$TARGETS" | xargs) "$RKTMDIR" || exit 1
-cp rktm "$RKTMBIN" && echo "Done."
+cp rktm "$BINDIR/rktm" && echo "Done."
+
+additionally=$(cat <<EOF
+
+Additionally, in the BINDIR ($BINDIR), you could do:
+$ mv rktm rktm.rkt           # rename script
+$ raco exe -o rktm rktm.rkt  # compile a binary version
+EOF
+)
+echo "$additionally"
